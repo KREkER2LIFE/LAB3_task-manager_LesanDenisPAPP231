d72064176cc80e8d94431d4a1c7eeb48aa553554
Dodogo <denislesan2022@gmail.com>
2026-09-18 09:58:21 +0300

    Sarcina 6 - Adaugarea sarcinilor in array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import TaskForm from "./components/TaskForm";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(taskName) {
+    if (taskName.trim() === "") {
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      title: taskName,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <main>
       <h1>Task Manager</h1>
-      <TaskForm />
+
+      <TaskForm onAddTask={addTask} />
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
