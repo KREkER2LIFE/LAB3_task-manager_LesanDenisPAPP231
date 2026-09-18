18d4e08a98d1115598e3e9de806c9a82c7374086
Dodogo <denislesan2022@gmail.com>
2026-09-18 10:16:35 +0300

    Sarcina 7 - Adaugarea sarcinilor introduse in input

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
-function TaskForm() {
+function TaskForm({ onAddTask }) {
   const [taskName, setTaskName] = useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onAddTask(taskName);
+
+    setTaskName("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Introdu denumirea sarcinii"
